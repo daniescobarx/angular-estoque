2eb370894f4a4379e71860d1b763d7465a00eb15
daniescobarx <seu-email@exemplo.com>
2024-02-26 12:33:21 -0300

    validators

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-product',
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.css'
 })
-export class AddEditProductComponent {
+export class AddEditProductComponent implements OnInit{
+    form: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+      this.form = this.fb.group({
+        name: ['', [Validators.required, Validators.maxLength(10)]],
+        description: ['', Validators.required],
+        price: [null, Validators.required],
+        stock: [null, Validators.required],
+      })
+    }
+
+    ngOnInit(): void {
+
+    }
 }
