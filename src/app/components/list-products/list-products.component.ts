2e25ef3d06e3e59b9e4231538609ab29a9bc080d
daniescobarx <seu-email@exemplo.com>
2024-02-25 22:18:41 -0300

    add listProducts

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-list-products',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './list-products.component.css'
 })
 export class ListProductsComponent {
+  listProducts: Product[] = [
+    {name: "Banana", description: "Fruta amarela e tals", price: 12, stock:200},
+    {name: "Vodka", description: "bebida", price: 20, stock:100},
 
+  ]
 }
